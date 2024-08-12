5f1906804c9ff61f35a6e8f06b8ff9c3a400efeb
Sway <sway@milesight.com>
2024-08-12 16:08:54 +0800

    feat(em310-udl): support encoder

---
+++ EM_Series/EM300_Series/EM310-UDL/EM310-UDL_Encoder.js
@@ -0,0 +1,186 @@
+/**
+ * Payload Encoder
+ *
+ * Copyright 2024 Milesight IoT
+ *
+ * @product EM310-UDL
+ */
+// Chirpstack v4
+function encodeDownlink(input) {
+    var encoded = milesightDeviceEncode(input.data);
+    return { bytes: encoded };
+}
+
+// Chirpstack v3
+function Encode(fPort, obj) {
+    return milesightDeviceEncode(obj);
+}
+
+// The Things Network
+function Encoder(obj, port) {
+    return milesightDeviceEncode(obj);
+}
+
+function milesightDeviceEncode(payload) {
+    var encoded = [];
+
+    if ("reboot" in payload) {
+        encoded = encoded.concat(reboot(payload.reboot));
+    }
+    if ("collection_interval" in payload) {
+        encoded = encoded.concat(setCollectionInterval(payload.collection_interval));
+    }
+    if ("report_interval" in payload) {
+        encoded = encoded.concat(setReportInterval(payload.report_interval));
+    }
+    if ("distance_threshold_config" in payload) {
+        encoded = encoded.concat(setDistanceThresholdConfig(payload.distance_threshold_config.condition, payload.distance_threshold_config.min, payload.distance_threshold_config.max, payload.distance_threshold_config.lock_time, payload.distance_threshold_config.continue_time));
+    }
+
+    return encoded;
+}
+
+/**
+ * reboot device
+ * @param {number} reboot values: (0: "no", 1: "yes")
+ * @example { "reboot": 1 }
+ */
+function reboot(reboot) {
+    var reboot_values = [0, 1];
+    if (reboot_values.indexOf(reboot) === -1) {
+        throw new Error("reboot must be one of " + reboot_values.join(", "));
+    }
+
+    if (reboot === 0) {
+        return [];
+    }
+    return [0xff, 0x10, 0xff];
+}
+
+/**
+ * set collection interval
+ * @param {number} collection_interval unit: second, range: [10, 60]
+ * @example { "collection_interval": 300 }
+ */
+function setCollectionInterval(collection_interval) {
+    if (typeof collection_interval !== "number") {
+        throw new Error("collection_interval must be a number");
+    }
+    if (collection_interval < 10 || collection_interval > 60) {
+        throw new Error("collection_interval must be in range [10, 60]");
+    }
+
+    var buffer = new Buffer(4);
+    buffer.writeUInt8(0xff);
+    buffer.writeUInt8(0x02);
+    buffer.writeUInt16LE(collection_interval);
+    return buffer.toBytes();
+}
+
+/**
+ * report interval configuration
+ * @param {number} report_interval uint: second
+ * @example payload: { "report_interval": 600 }
+ */
+function setReportInterval(report_interval) {
+    if (typeof report_interval !== "number") {
+        throw new Error("report_interval must be a number");
+    }
+    if (report_interval < 1) {
+        throw new Error("report_interval must be greater than 1");
+    }
+
+    var buffer = new Buffer(4);
+    buffer.writeUInt8(0xff);
+    buffer.writeUInt8(0x03);
+    buffer.writeUInt16LE(report_interval);
+    return buffer.toBytes();
+}
+
+/**
+ * set distance threshold alarm configuration
+ * @param {number} condition values: (0: disable, 1: below, 2: above, 3: between, 4: outside)
+ * @param {number} min condition=(below, within, outside)
+ * @param {number} max condition=(above, within, outside)
+ * @param {number} continue_time unit: minute
+ */
+function setDistanceThresholdConfig(condition, min, max, lock_time, continue_time) {
+    var condition_values = [0, 1, 2, 3, 4];
+    if (condition_values.indexOf(condition) === -1) {
+        throw new Error("condition must be one of " + condition_values.join(", "));
+    }
+
+    var data = condition | (1 << 3);
+
+    var buffer = new Buffer(11);
+    buffer.writeUInt8(0xff);
+    buffer.writeUInt8(0x06);
+    buffer.writeUInt8(data);
+    buffer.writeInt16LE(min * 10);
+    buffer.writeInt16LE(max * 10);
+    buffer.writeUInt16LE(lock_time);
+    buffer.writeUInt16LE(continue_time);
+    return buffer.toBytes();
+}
+
+function Buffer(size) {
+    this.buffer = new Array(size);
+    this.offset = 0;
+
+    for (var i = 0; i < size; i++) {
+        this.buffer[i] = 0;
+    }
+}
+
+Buffer.prototype._write = function (value, byteLength, isLittleEndian) {
+    for (var index = 0; index < byteLength; index++) {
+        var shift = isLittleEndian ? index << 3 : (byteLength - 1 - index) << 3;
+        this.buffer[this.offset + index] = (value & (0xff << shift)) >> shift;
+    }
+};
+
+Buffer.prototype.writeUInt8 = function (value) {
+    this._write(value, 1, true);
+    this.offset += 1;
+};
+
+Buffer.prototype.writeInt8 = function (value) {
+    this._write(value < 0 ? value + 0x100 : value, 1, true);
+    this.offset += 1;
+};
+
+Buffer.prototype.writeUInt16LE = function (value) {
+    this._write(value, 2, true);
+    this.offset += 2;
+};
+
+Buffer.prototype.writeInt16LE = function (value) {
+    this._write(value < 0 ? value + 0x10000 : value, 2, true);
+    this.offset += 2;
+};
+
+Buffer.prototype.writeUInt32LE = function (value) {
+    this._write(value, 4, true);
+    this.offset += 4;
+};
+
+Buffer.prototype.writeInt32LE = function (value) {
+    this._write(value < 0 ? value + 0x100000000 : value, 4, true);
+    this.offset += 4;
+};
+
+Buffer.prototype.writeD2DCommand = function (value, defaultValue) {
+    if (typeof value !== "string") {
+        value = defaultValue;
+    }
+    if (value.length !== 4) {
+        throw new Error("d2d_cmd length must be 4");
+    }
+    this.buffer[this.offset] = parseInt(value.substr(2, 2), 16);
+    this.buffer[this.offset + 1] = parseInt(value.substr(0, 2), 16);
+    this.offset += 2;
+};
+
+Buffer.prototype.toBytes = function () {
+    return this.buffer;
+};
